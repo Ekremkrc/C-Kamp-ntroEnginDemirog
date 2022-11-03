7d8ef945f4ec5816010ffca33d2b7294d6f9d74f
Ekrem Berkay Karaca <berkaykrc07@gmail.com>
2022-11-03 06:33:04 +0300

    3.gün ödev eklendi.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " İsimli kişi müşteri listenize eklendi.");
+        }
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " İsimli kişi müşteri listenizden silinmiştir.");
+        }
+        public void Get(Customer customer)
+        {
+            Console.WriteLine("Id: " + customer.Id + ", Ad:" + customer.Name + ", Soyad: " + customer.Surname);
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassMethodDemo;
+
+Customer customer1 = new Customer();
+customer1.Id = 1;
+customer1.Name = "Metin";
+customer1.Surname = "Karaca";
+
+Customer customer2 = new Customer();
+customer2.Id = 2;
+customer2.Name = "Fatma";
+customer2.Surname = "Karaca";
+
+Customer customer3 = new Customer();
+customer3.Id = 3;
+customer3.Name = "Mert";
+customer3.Surname = "Karaca";
+
+Customer customer4 = new Customer();
+customer4.Id = 4;
+customer4.Name = "Bahadır";
+customer4.Surname = "Karaca";
+
+Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
+
+CustomerManager customerManager = new CustomerManager();
+
+foreach (Customer customer in customers)
+{
+    customerManager.Add(customer); //Ekle
+    customerManager.Remove(customer); //Sil
+    customerManager.Get(customer); //Listele
+    Console.WriteLine("------------");
+
+}
