de952d0e4e3bee0c4dc97c760617d10f2ba88d4f
Ekrem Berkay Karaca <berkaykrc07@gmail.com>
2022-11-05 18:08:17 +0300

    Constructor Konu

---
+++ Constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Customer customer1 = new Customer(1,"Berkay","Karaca","Ankara");
+
+
+Console.WriteLine(customer1.FirstName);
+
+
+
+class Customer
+{
+   
+    
+    //default Constructor
+    public Customer(int id, string firstName, string lastName, string city)
+    {
+        ID = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+    
+    
+    
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+
+}
+
+
+
+
+
+
+
