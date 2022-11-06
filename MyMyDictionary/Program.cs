cc6507a008c5e9263dde2e0dae1ea5a471daa018
Ekrem Berkay Karaca <berkaykrc07@gmail.com>
2022-11-06 15:16:44 +0300

    MyDictionary - HomeWork

---
+++ MyMyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyMyDictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDictionary<string> ulkeler = new MyDictionary<string>();
+        ulkeler.Add("Türkiye");
+        ulkeler.Add("İngiltere");
+        ulkeler.Add("Fransa");
+        ulkeler.Add("Almanya");
+        ulkeler.Add("USA");
+
+    }
+}
---
+++ OOP2/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoP2
+{
+    internal class Customer
+    {
+    }
+}
