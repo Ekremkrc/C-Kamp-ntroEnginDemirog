261c140922aae311226eaefdd8dec96ec323da46
Ekrem Berkay Karaca <berkaykrc07@gmail.com>
2022-11-25 16:00:33 +0300

    LinqPratic

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 List<Category> categories = new List<Category>
   {
@@ -16,47 +17,103 @@ List<Product> products = new List<Product>
 
 };
 
-Console.WriteLine("Algoritmik...............");
+//Test(products);
+//AnyTest(products); bir listenin içersinde eleman var mı yok mu ara?
+//FindTest(products); ürünün detayına gitmek için
+//FindAllTest(products); where gibi koşul sağlar
+//AscDescTest(products); detaylı sıralama
+//ClassicLinqTest(products);
 
-foreach (var product in products)
+var result = from p in products
+             join c in categories
+             on p.CategoryId equals c.CategoryId
+             where p.UnitPrice >5000
+             orderby p.UnitPrice descending
+             select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+foreach (var productDto in result)
 {
-    if (product.UnitPrice>5000 && product.UnitsInStock > 3)
-    {
-        Console.WriteLine(product.ProductName);
-    }
+    Console.WriteLine("{0} --- {1}", productDto.ProductName, productDto.CategoryName);
 }
 
 
-Console.WriteLine("Linqq-----------------");
-var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3);
-foreach (var p in result)
-{
-    Console.WriteLine(p.ProductName);
-}
-//Algoritmik gerçek hayat
-static List<Product> GetProducts(List<Product> products)
-{
-    List<Product> filteredProducts = new List<Product>();
 
-    foreach (var product in products)
-    {
-        if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
-        {
-            filteredProducts.Add(product);
-        }
 
-    }
 
-    return filteredProducts;
-}
+
 //Linq gerçek hayat
 static List<Product> GetProductsLinq(List<Product> products)
 {
     return products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3).ToList();
 }
-    
+
+static void Test(List<Product> products)
+{
+    Console.WriteLine("Algoritmik...............");
+
+    foreach (var product in products)
+    {
+        if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
 
 
+    Console.WriteLine("Linqq-----------------");
+    var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3);
+    foreach (var p in result)
+    {
+        Console.WriteLine(p.ProductName);
+    }
+}
+
+static void AnyTest(List<Product> products)
+{
+    var result = products.Any(p => p.ProductName == "Acer Laptop");
+    Console.WriteLine(result);
+}
+
+static void FindTest(List<Product> products)
+{
+    var result = products.Find(p => p.ProductId == 3);
+    Console.WriteLine(result.ProductName);
+}
+
+static void FindAllTest(List<Product> products)
+{
+    var result = products.FindAll(p => p.ProductName.Contains("top"));
+    Console.WriteLine(result);
+}
+
+static void NewMethod(List<Product> products)
+{
+    var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+    foreach (var product in result)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
+
+static void ClassicLinqTest(List<Product> products)
+{
+    var result = from p in products
+                 where p.UnitPrice > 6000
+                 orderby p.UnitPrice descending, p.ProductName descending
+                 select new ProductDto { ProductName = p.ProductName, ProductId = p.ProductId, UnitPrice = p.UnitPrice, };
+    foreach (var product in result)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
+
+class ProductDto
+{
+
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal UnitPrice { get; set; }
+    public string CategoryName { get; set; }
+}
 
 class Product
 {
@@ -64,11 +121,11 @@ class Product
     public string ProductName { get; set; }
     public string QuantityPerUnit { get; set; }
     public decimal UnitPrice { get; set; }
-    public int UnitsInStock { get; set;}
+    public int UnitsInStock { get; set; }
     public int CategoryId { get; set; }
 }
 
-  class Category
+class Category
 {
     public int CategoryId { get; set; }
     public string CategoryName { get; set; }
